431aeb7583fc6edb2659220a4d6be0791ec18443
Guillermo Osuna <gosuna79@gmail.com>
2015-10-20 22:40:52 -0600

    cambios en nav de nuevo

--- js/nav.js
+++ js/nav.js
@@ -9,8 +9,22 @@ $(document).scroll(function() {
   $('nav').css('background', 'rgba(255, 255, 255,' + $(this).scrollTop() / $(window).height() + ')');
   $('#cabezera nav').css('box-shadow', '2px 1px 3px rgba(150,150,150,' + $(this).scrollTop() / $(window).height() + ')');
 
-  	
-  	if($(this).scrollTop() / $(window).height()>=.0 && $(this).scrollTop() / $(window).height()<=.2){
+  }	
+		if (matchMedia) {
+			var mq = window.matchMedia("(max-width: 920px)");
+			mq.addListener(WidthChange);
+			WidthChange(mq);
+		}
+
+		
+		function WidthChange(mq) {
+
+			if (mq.matches) {
+				$('.navi').css('color', 'rgba(0,0,0,1)');	
+			}
+			else {
+				$('.navi').css('color', 'rgba(255,255,255,1)');
+					if($(this).scrollTop() / $(window).height()>=.0 && $(this).scrollTop() / $(window).height()<=.2){
 	  	$('#cabezera nav ul li a').css('color', 'rgba(255,255,255,1)');
 	  	$('.navi').css('color', 'rgba(255,255,255,1)');
 	  		$(function() {
@@ -21,10 +35,6 @@ $(document).scroll(function() {
 			    $('.navi').css('color', 'rgba(255,255,255,1)');
 			  });
 			});
-
-
-
-
 
 	  		$(function() {
 			  $('#cabezera nav ul li #1').hover(function() {
@@ -78,168 +88,7 @@ $(document).scroll(function() {
 
 
 
-	  if($(this).scrollTop() / $(window).height()>=.21 && $(this).scrollTop() / $(window).height()<=.3){
-	  	$('#cabezera nav ul li a').css('color', 'rgba(200,200,200,1)');
-	  		$(function() {
-			  $('#cabezera nav ul li #1').hover(function() {
-			    $('#cabezera nav ul li #1').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #1').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #2').hover(function() {
-			    $('#cabezera nav ul li #2').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #2').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #3').hover(function() {
-			    $('#cabezera nav ul li #3').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #3').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #4').hover(function() {
-			    $('#cabezera nav ul li #4').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #4').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #5').hover(function() {
-			    $('#cabezera nav ul li #5').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #5').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #6').hover(function() {
-			    $('#cabezera nav ul li #6').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #6').css('color', 'rgba(200,200,200,1)');
-			  });
-			});
-	  	  }
-
-
-
-
-	  if($(this).scrollTop() / $(window).height()>=.31 && $(this).scrollTop() / $(window).height()<=.5){
-	  	$('#cabezera nav ul li a').css('color', 'rgba(120,120,120,1)');
-	  		$(function() {
-			  $('#cabezera nav ul li #1').hover(function() {
-			    $('#cabezera nav ul li #1').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #1').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #2').hover(function() {
-			    $('#cabezera nav ul li #2').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #2').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #3').hover(function() {
-			    $('#cabezera nav ul li #3').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #3').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #4').hover(function() {
-			    $('#cabezera nav ul li #4').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #4').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #5').hover(function() {
-			    $('#cabezera nav ul li #5').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #5').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #6').hover(function() {
-			    $('#cabezera nav ul li #6').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #6').css('color', 'rgba(120,120,120,1)');
-			  });
-			});
-	  }
-
-
-
-
-
-	  if($(this).scrollTop() / $(window).height()>=.51 && $(this).scrollTop() / $(window).height()<=.7){
-	  	$('#cabezera nav ul li a').css('color', 'rgba(80,80,80,1)');
-	  		$(function() {
-			  $('#cabezera nav ul li #1').hover(function() {
-			    $('#cabezera nav ul li #1').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #1').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #2').hover(function() {
-			    $('#cabezera nav ul li #2').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #2').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #3').hover(function() {
-			    $('#cabezera nav ul li #3').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #3').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #4').hover(function() {
-			    $('#cabezera nav ul li #4').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #4').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #5').hover(function() {
-			    $('#cabezera nav ul li #5').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #5').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  	$(function() {
-			  $('#cabezera nav ul li #6').hover(function() {
-			    $('#cabezera nav ul li #6').css('color', 'rgba(99,56,0,1)');
-			  }, function() {
-			    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-			    $('#cabezera nav ul li #6').css('color', 'rgba(80,80,80,1)');
-			  });
-			});
-	  }
+	
 
 
 
@@ -257,9 +106,6 @@ $(document).scroll(function() {
 			    $('.navi').css('color', 'rgba(0,0,0,1)');
 			  });
 			});
-
-
-
 	  		$(function() {
 			  $('#cabezera nav ul li #1').hover(function() {
 			    $('#cabezera nav ul li #1').css('color', 'rgba(99,56,0,1)');
@@ -309,11 +155,13 @@ $(document).scroll(function() {
 			  });
 			});
 	  }
-  }
+			}
 
-		
+		}
 
 
+
+			
 });
 
 
