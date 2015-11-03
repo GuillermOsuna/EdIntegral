ba28614a0e9ef12904bd2070fb3e94e8a61db2cf
saile patricia moreno villegas <sp_morvi@hotmail.com>
2015-11-02 21:08:15 -0700

    js servicios

--- js/servicios.js
+++ js/servicios.js
@@ -1,37 +1,53 @@
 $( document ).ready(function() {
 
-	$(".section_rgba1").mouseenter(function(){
-    $('.p1').show();
-			$('.h31').hide();
-			});
-	$(".section_rgba1").mouseleave(function(){
+	var timer;
+$(".section_rgba1").mouseenter( function(){
+    timer = setTimeout(function () {
+       $('.p1').show();
+			$('.h31').hide();;
+    }, 350);
+}).mouseleave( function(){
+    clearTimeout(timer);
     $('.p1').hide();
 			$('.h31').show();
-			});
-
-	$(".section_rgba2").mouseenter(function(){
-    $('.p2').show();
-			$('.h32').hide();
-			});
-	$(".section_rgba2").mouseleave(function(){
+});
+var timer;
+$(".section_rgba2").mouseenter( function(){
+    timer = setTimeout(function () {
+       $('.p2').show();
+			$('.h32').hide();;
+    }, 350);
+}).mouseleave( function(){
+    clearTimeout(timer);
     $('.p2').hide();
 			$('.h32').show();
-			});
-	$(".section_rgba3").mouseenter(function(){
-    $('.p3').show();
-			$('.h33').hide();
-			});
-	$(".section_rgba3").mouseleave(function(){
+});
+var timer;
+$(".section_rgba3").mouseenter( function(){
+    timer = setTimeout(function () {
+       $('.p3').show();
+			$('.h33').hide();;
+    }, 350);
+}).mouseleave( function(){
+    clearTimeout(timer);
     $('.p3').hide();
 			$('.h33').show();
-			});
-	$(".section_rgba4").mouseenter(function(){
-    $('.p4').show();
-			$('.h34').hide();
-			});
-	$(".section_rgba4").mouseleave(function(){
+});
+var timer;
+$(".section_rgba4").mouseenter( function(){
+    timer = setTimeout(function () {
+       $('.p4').show();
+			$('.h34').hide();;
+    }, 350);
+}).mouseleave( function(){
+    clearTimeout(timer);
     $('.p4').hide();
 			$('.h34').show();
-			});
+});
+
+
+
+
+	
 });
 
