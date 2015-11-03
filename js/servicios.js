bc394dddee7c276a1ede195b5e0433a9475740df
saile patricia moreno villegas <sp_morvi@hotmail.com>
2015-11-02 20:54:06 -0700

    servicios

---
+++ js/servicios.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+	$(".section_rgba1").mouseenter(function(){
+    $('.p1').show();
+			$('.h31').hide();
+			});
+	$(".section_rgba1").mouseleave(function(){
+    $('.p1').hide();
+			$('.h31').show();
+			});
+
+	$(".section_rgba2").mouseenter(function(){
+    $('.p2').show();
+			$('.h32').hide();
+			});
+	$(".section_rgba2").mouseleave(function(){
+    $('.p2').hide();
+			$('.h32').show();
+			});
+	$(".section_rgba3").mouseenter(function(){
+    $('.p3').show();
+			$('.h33').hide();
+			});
+	$(".section_rgba3").mouseleave(function(){
+    $('.p3').hide();
+			$('.h33').show();
+			});
+	$(".section_rgba4").mouseenter(function(){
+    $('.p4').show();
+			$('.h34').hide();
+			});
+	$(".section_rgba4").mouseleave(function(){
+    $('.p4').hide();
+			$('.h34').show();
+			});
+});
+
